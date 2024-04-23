706c97137c22b731810c491c650781633dccf0fd
Lucas <lrasilvestre__@outlook.com>
2024-04-23 20:13:47 +0100

    aTUALIZAÇÃO INICIO php

---
+++ UFCD_5416/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+include 'teste/head.php';
+include 'teste/header.php';
+include 'teste/nav.php';
+include 'teste/main.php';
+include 'teste/footer.php';
+
+
+
+?>
---
+++ UFCD_5416/teste/conteudo1.php
@@ -0,0 +1,24 @@
+<form method="post">
+    <div class="form-floating mb-3">
+        <input class="form-control" id="inputEmail" type="text" name="nome" placeholder="name@example.com" />
+        <label for="inputEmail">Nome</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input class="form-control" id="inputPassword" type="password" name="senha" placeholder="Password" />
+        <label for="inputPassword">Password</label>
+    </div>
+    <div class="form-check mb-3">
+        <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
+        <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
+    </div>
+    <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+        <a class="small" href="password.html">Forgot Password?</a>
+        <button type="submit" name="login" class="btn btn-primary">Login</button>
+    </div>
+</form>
+
+<?php 
+	if(isset($_POST['login'])){
+		login_test($_POST["nome"], $_POST["senha"]);
+	}
+?>
