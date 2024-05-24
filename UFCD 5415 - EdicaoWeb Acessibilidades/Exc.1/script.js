d33c48051239468045f462046f94ea47bebe936c
marianatavares1 <marianatavares__@hotmail.com>
2024-05-24 16:43:17 +0100

    Exercicio 1 finalizado

---
+++ UFCD 5415 - EdicaoWeb Acessibilidades/Exc.1/script.js
@@ -0,0 +1,7 @@
+function escrever(){
+    document.getElementById("addTexto").innerHTML = "Texto escrito no div da esquerda"
+}
+
+function apagarTexto() {
+    document.getElementById("addTexto").innerHTML = ""
+}
